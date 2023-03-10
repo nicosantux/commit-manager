5a771433adb7d5402daeab3f96830339a27bb528
Nicolás Santuccio <nico.santuccio@gmail.com>
2023-03-09 22:50:53 -0300

    🐛 fix: import order

--- src/utils/git.utils.ts
+++ src/utils/git.utils.ts
@@ -4,10 +4,10 @@ import fs from 'node:fs/promises'
 import path from 'node:path'
 import { fileURLToPath } from 'node:url'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
-
 import { outro } from '@clack/prompts'
+
+const __filename = path.dirname(fileURLToPath(import.meta.url))
+const __dirname = path.dirname(__filename)
 
 const execAsync = promisify(exec)
 
