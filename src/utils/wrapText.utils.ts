04facbec3ad7fd3aae6e650f5cccb41e91b3bb72
Nicolás Santuccio <nico.santuccio@gmail.com>
2023-03-11 14:53:00 -0300

    🔄 change: remove offset default value
    this parameter will always be sent by yargs default text wrap

--- src/utils/wrapText.utils.ts
+++ src/utils/wrapText.utils.ts
@@ -1,4 +1,4 @@
-export const wrapText = (text: string, offset: number = 72) =>
+export const wrapText = (text: string, offset: number) =>
   text
     .replaceAll('\\n', '\n')
     .replace(new RegExp(`(?![^\\n]{1,${offset}}$)([^\\n]{1,${offset}})\\s`, 'g'), '$1\n')
